19a7b288d0337e9b40bf13d77b7b1e3f723eca8b
lanebo1 <k.efimovich@innopolis.university>
2024-08-12 15:06:50 +0300

    fix: buttons

--- src/components/shared/buttons/Orange Button/OrangeButton.tsx
+++ src/components/shared/buttons/Orange Button/OrangeButton.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import openLink from '@/functions/openLink'
 import { ButtonProps } from '@/types/ButtonProps'
 
-function OrangeButton({ text, link }: ButtonProps) {
+const OrangeButton: React.FC<ButtonProps> = ({ text, link, className }) => {
   return (
     <button
-      className="flex items-center justify-center rounded-full bg-color-orange px-8 py-6 text-sm font-semibold text-white"
+      className={`flex items-center justify-center rounded-full bg-color-orange px-8 py-6 text-sm font-semibold text-white ${className}`}
       onClick={() => link && openLink(link)}
     >
       <p>{text}</p>
--- src/types/ButtonProps.ts
+++ src/types/ButtonProps.ts
@@ -3,6 +3,5 @@ import openLink from '@/functions/openLink'
 export interface ButtonProps {
   text: string
   link?: string
-  function?: void | openLink
-  type?: string
+  className?: string
 }
